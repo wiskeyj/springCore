462ffae8c4b78431a6b59a3755f1aa1587a3c988
wiskeyj <wxyzi1226@gmail.com>
2021-02-07 15:22:50 +0900

    관심사 분리

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,15 +1,19 @@
 package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy disocuntPolicy = new FixDiscountPolicy();
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy disocuntPolicy;
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy disocuntPolicy) {
+        this.memberRepository = memberRepository;
+        this.disocuntPolicy = disocuntPolicy;
+    }
+
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
